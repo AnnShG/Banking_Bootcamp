afa7f33f5ddfffd6d8620f60475f012f508c3bed
Anna Shkaeva <anna.shkajeva@gmail.com>
2024-03-27 13:31:15 +0200

    Completion of the task

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -4,4 +4,56 @@ public class BankAccount {
     public BankAccount(int balance) {
         this.balance = balance;
     }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+
+
+        // add catch & try for catching the exceptions
+//        while(true) {
+//            try {
+//                balance += amount;
+//            }
+//            catch(IllegalArgumentException e) {
+//                System.out.println("Amount for increasing should be greater than zero");
+//            }
+//        }
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount for increasing should be greater than zero");
+        } else {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount for decreasing should be greater than zero");
+        } else {
+            balance -= amount;
+        }
+
+    }
+
+    public void printBalance() {
+        System.out.println("The current balance: " + balance);
+
+    }
+
+//    method for transfer balance from one bank account to another
+    public void transferBalance(int amount, BankAccount transferBalanceTo) {
+    if (amount <= 0) {
+        System.out.println("Transfer amount should be greater than zero");
+        return;
+    }
+    if (this.balance < amount) {
+        System.out.println("Your balance is less than the amount you want to transfer");
+    }
+
+    this.balance -= amount;
+        transferBalanceTo.deposit(amount);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 //create a class "BankAccount" with property "balance" (should be decimal number)
-//
 //create an empty default constructor for it
 //create a constructor with parameter for setting balance.
 //create a method "deposit" with one parameter "amount" (decimal number)
@@ -9,8 +8,42 @@
 //method for transfer balance from one bank account to another
 
 //** Optionally make the program interactive with user e.g. using Scanner
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        BankAccount one = new BankAccount(150);
+        BankAccount two = new BankAccount(0); // an empty to transfer to
+
+        // a menu for the user to decide if he wants to increase, decrease or transfer
+
+
+        System.out.print("Enter the amount to increase your balance: ");
+        int increaseBalance = scanner.nextInt();
+        one.deposit(increaseBalance);
+
+        one.printBalance();
+
+        System.out.print("Enter the amount to decrease your balance: ");
+        int decreaseBalance = scanner.nextInt();
+        one.withdraw(decreaseBalance);
+
+        one.printBalance();
+
+        System.out.print("Enter the amount you want to transfer from one account to another: ");
+        int transferAmount = scanner.nextInt();
+
+        one.transferBalance(transferAmount, two);
+
+        one.printBalance();
+        two.printBalance();
+
+
+
+
+
+
     }
 }
