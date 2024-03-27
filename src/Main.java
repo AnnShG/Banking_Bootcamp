c4afc7db82218453b5adfa60f901234db9cfb7e6
Anna Shkaeva <anna.shkajeva@gmail.com>
2024-03-27 09:54:08 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//create a class "BankAccount" with property "balance" (should be decimal number)
+//
+//create an empty default constructor for it
+//create a constructor with parameter for setting balance.
+//create a method "deposit" with one parameter "amount" (decimal number)
+//which allows the user to increase the balance.
+//create a method "withdraw" with one parameter "amount"(decimal number) which allows the user to decrease the balance.
+//create a method "printBalance" which displays the current balance to user
+//method for transfer balance from one bank account to another
+
+//** Optionally make the program interactive with user e.g. using Scanner
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
